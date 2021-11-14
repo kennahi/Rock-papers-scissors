9304a469caad7dc456a46a8e4682d0b2cb344c05
kennahi <khaldoun.ennahi@gmail.com>
2021-11-14 22:43:35 +0100

    added visual UI with interactions

--- rps.js
+++ rps.js
@@ -1,3 +1,15 @@
+const btn = document.querySelectorAll(`[data-select]`);
+const playerScore = document.querySelector(".playerScoreNumber");
+const computerScore = document.querySelector(".computerScoreNumber");
+const roundResult = document.querySelector(".roundResult");
+const roundNumber = document.querySelector(".roundNumber");
+
+let k = 0;
+let i = 0;
+let j = 0;
+let ret = 0;
+let round = 0;
+
 function computerPlay() {
     let random = Math.floor(Math.random() * 3) + 1;
     let move;
@@ -11,44 +23,91 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors')
-        return "error";
-    console.log("you played ==========> " + playerSelection);
-    console.log("the computer played => " + computerSelection);
-    if (playerSelection === computerSelection) 
+    if (playerSelection === computerSelection)
         return "its a draw";
-    if (playerSelection === 'rock' && computerSelection === 'paper')
+    else if (playerSelection === 'rock' && computerSelection === 'paper')
         return "you lose";
-    if (playerSelection === 'rock' && computerSelection === 'scissors')
+    else if (playerSelection === 'rock' && computerSelection === 'scissors')
         return "you win";
-    if (playerSelection === 'paper' && computerSelection === 'rock')
+    else if (playerSelection === 'paper' && computerSelection === 'rock')
         return "you win";
-    if (playerSelection === 'paper' && computerSelection === 'scissors')
+    else if (playerSelection === 'paper' && computerSelection === 'scissors')
         return "you lose";
-    if (playerSelection === 'scissors' && computerSelection === 'paper')
+    else if (playerSelection === 'scissors' && computerSelection === 'paper')
         return "you win";
-    if (playerSelection === 'scissors' && computerSelection === 'rock')
+    else if (playerSelection === 'scissors' && computerSelection === 'rock')
         return "you lose";
-    return ;
+    return;
 }
-function game() {
-    let str;
-    let i = 0;
-    let j = 0;
-    for (let k = 0; k < 5; k++) {
-        str = playRound(prompt("choose a move", "rock/paper/scissors"), computerPlay());
-        console.log(str);
-        if (str === "error" || str === "its a draw")
+
+
+btn.forEach((button) => {
+    button.addEventListener("click", () => {
+        const selectionName = button.dataset.select;
+        let str;
+
+
+        if (k == 0)
+            ret = 0;
+        if (k == 5) {
+            return;
+        }
+        str = playRound(selectionName, computerPlay());
+
+        if (str === "its a draw") {
             k--;
-        else if (str === "you win")
+            round--;
+            roundResult.textContent = 'DRAW';
+        }
+        else if (str === "you win") {
             i++;
-        else if (str === "you lose")
-            j--;
+            playerScore.textContent = i;
+            roundResult.textContent = 'WIN';
+        }
+        else if (str === "you lose") {
+            j++;
+            computerScore.textContent = j;
+            roundResult.textContent = 'LOSE';
+        }
+        round++;
+        roundNumber.textContent = round;
+
+        k++;
+
+        if (k == 5) {
+            if (i > j)
+                roundResult.textContent = 'congratulations you won!'
+            else
+                roundResult.textContent = 'you lost lol'
+            if (ret == 0) {
+                retr();
+                ret = 1;
+            }
+        }
+    })
+})
+
+function retr() {
+    const restart = document.querySelector('.retryButton');
+    const retryButton = document.querySelector(".buttonDiv");
+    const retry = document.createElement('button');
+
+    retry.setAttribute('id', 'retryButton');
+    retry.textContent = 'Retry?';
+    retryButton.appendChild(retry);
+    if (retry) {
+        retry.addEventListener('click', function () {
+            const el = document.getElementById('retryButton');
+            el.remove();
+            k = 0;
+            roundResult.textContent = '--';
+            playerScore.textContent = '0';
+            computerScore.textContent = '0';
+            roundNumber.textContent = '1';
+            i = 0;
+            j = 0;
+            round = 0;
+        })
     }
-    if (i > j)
-            console.log("congratulations, you won");
-        else
-            console.log("you SUCK");
+
 }
-game();
